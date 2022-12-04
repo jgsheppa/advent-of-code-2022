a677ff49648c6f1dfa6c061243f1008fb02ebf84
jgsheppa <james.sheppard@avisio.ai>
2022-12-04 16:21:58 +0100

    complete challenge 2 day 4

--- dayFour/day_four.go
+++ dayFour/day_four.go
@@ -11,6 +11,49 @@ type Section struct {
 	end   int
 }
 
+func CalculateEntireOverlappingSections(filename string) (int, error) {
+	testData, err := common.ReadFile(filename)
+	if err != nil {
+		return 0, err
+	}
+
+	overLappingSections := 0
+
+	for _, pair := range testData {
+		splitSections := strings.Split(pair, ",")
+
+		sectionsAsNumbers := make(map[int]Section)
+
+		for index, section := range splitSections {
+			split := strings.Split(section, "-")
+			start, err := strconv.Atoi(split[0])
+			if err != nil {
+				return 0, err
+			}
+			finish, err := strconv.Atoi(split[1])
+			if err != nil {
+				return 0, err
+			}
+
+			sectionsAsNumbers[index] = Section{
+				start,
+				finish,
+			}
+
+		}
+
+		switch true {
+		case sectionsAsNumbers[0].start >= sectionsAsNumbers[1].start && sectionsAsNumbers[0].end <= sectionsAsNumbers[1].end:
+			overLappingSections++
+			break
+		case sectionsAsNumbers[1].start >= sectionsAsNumbers[0].start && sectionsAsNumbers[1].end <= sectionsAsNumbers[0].end:
+			overLappingSections++
+			break
+		}
+	}
+	return overLappingSections, nil
+}
+
 func CalculateOverlappingSections(filename string) (int, error) {
 	testData, err := common.ReadFile(filename)
 	if err != nil {
@@ -43,6 +86,12 @@ func CalculateOverlappingSections(filename string) (int, error) {
 		}
 
 		switch true {
+		case sectionsAsNumbers[0].start >= sectionsAsNumbers[1].start && sectionsAsNumbers[0].start <= sectionsAsNumbers[1].end:
+			overLappingSections++
+			break
+		case sectionsAsNumbers[0].end <= sectionsAsNumbers[1].end && sectionsAsNumbers[0].end >= sectionsAsNumbers[1].start:
+			overLappingSections++
+			break
 		case sectionsAsNumbers[0].start >= sectionsAsNumbers[1].start && sectionsAsNumbers[0].end <= sectionsAsNumbers[1].end:
 			overLappingSections++
 			break
--- dayFour/day_four_test.go
+++ dayFour/day_four_test.go
@@ -2,12 +2,24 @@ package dayFour
 
 import "testing"
 
+func TestCalculateEntireOverlappingSections(t *testing.T) {
+	got, err := CalculateEntireOverlappingSections("test_input.txt")
+	if err != nil {
+		t.Errorf("test: %v error: %v", "CalculateEntireOverlappingSections", err)
+	}
+	want := 2
+
+	if got != want {
+		t.Errorf("got = %v want = %v", got, want)
+	}
+}
+
 func TestCalculateOverlappingSections(t *testing.T) {
 	got, err := CalculateOverlappingSections("test_input.txt")
 	if err != nil {
 		t.Errorf("test: %v error: %v", "CalculateOverlappingSections", err)
 	}
-	want := 3
+	want := 4
 
 	if got != want {
 		t.Errorf("got = %v want = %v", got, want)
